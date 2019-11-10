cfb74af4160e7278ed0c525a683b47e70200f3d7
Sam Waller <samuellwaller@gmail.com>
2019-11-10 04:32:54 -0500

    backend fetched to frontend

--- client/src/components/movies/movies.js
+++ client/src/components/movies/movies.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './movies.css';
+import React, { Component } from 'react';
+import './movies.css'
 
-function Movies() {
-  return (
-    <div>
-        <h2>Movies</h2>
-    </div>
-  );
+class Movies extends Component {
+  constructor(){
+    super()
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/movies')
+      .then(res => res.json())
+      .then(movies => this.setState({movies}, () => console.log("movies fetched:", movies)))
+  }
+
+  render() {
+    return (
+      <div>
+          <h2>Movies</h2>
+      </div>
+    )
+  }
+
 }
 
-export default Movies;
+export default Movies
